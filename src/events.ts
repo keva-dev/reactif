5e00bdd231f452ac4bf881b1d7cdf9c1f1f1bbb4
Tu Huynh <tu.huynh@linecorp.com>
2020-12-09 15:18:27 +0700

    fix event bugs

--- src/events.ts
+++ src/events.ts
@@ -1,10 +1,10 @@
 import { HandlerFunc } from './types'
 
-export function on(selector: string, eventName: string, handler: HandlerFunc) {
+export function on(selector: string, type: string, handler: HandlerFunc) {
   setTimeout(() => {
     const elem = document.querySelector(selector)
     if (!elem) return
-    elem.addEventListener(eventName, handler, { once: true })
+    elem.addEventListener(type, handler, false)
   }, 0)
 }
 
