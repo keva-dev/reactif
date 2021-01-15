ea83e2a289fa01a01bf3e2401e8b56c232787e5e
Tu Huynh <huynhminhtufu@gmail.com>
2021-01-15 17:22:44 +0700

    release 0.3.1 (#15)

---
+++ packages/router/src/types.ts
@@ -0,0 +1,19 @@
+export interface Data {
+  [key: string]: unknown
+}
+
+export interface ComponentObject {
+  components?: Record<string, ComponentObject>
+  setup?: (props?: Data, context?: Data) => Data
+  render?: () => string
+}
+
+export type HandlerFunc = () => void
+
+export interface RouterInstance {
+  renderer(render: (c: ComponentObject) => void, forceUnmount: (c: ComponentObject) => void): void
+  routerContextFn: () => {
+    params: () => Record<string, string>,
+    go: (path: string) => void
+  }
+}
